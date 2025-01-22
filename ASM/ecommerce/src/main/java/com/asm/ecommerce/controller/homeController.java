078d76aaf6512ee456ef9c09e60ba077bbf00847
kuuki1 <phamtin010900@gmail.com>
2025-01-22 19:06:46 +0700

    Lab3

---
+++ ASM/ecommerce/src/main/java/com/asm/ecommerce/controller/homeController.java
@@ -0,0 +1,23 @@
+package com.asm.ecommerce.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class homeController {
+    
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register(){
+        return "register";
+    }
+}
---
+++ LAB/demo/src/main/java/com/example/Lab3/controller/staffController.java
@@ -0,0 +1,64 @@
+package com.example.Lab3.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.Lab3.entity.Staff;
+
+@Controller
+@RequestMapping
+public class staffController {
+    @RequestMapping("/staff/detail")
+    public String detail(Model model) {
+        Staff staff = Staff.builder()
+                .id("user@gmail.com")
+                .fullname("nguyễn văn user")
+                .level(2)
+                .build();
+        model.addAttribute("staff", staff);
+        return "staff-detail";
+    }
+
+    @RequestMapping("/staff/list")
+    public String list(Model model) {
+        List<Staff> list = List.of(
+                Staff.builder().id("user1@gmail.com").fullname("nguyễn văn user1").level(0).build(),
+                Staff.builder().id("user2@gmail.com").fullname("nguyễn văn user2").level(1).build(),
+                Staff.builder().id("user3@gmail.com").fullname("nguyễn văn user3").level(2).build(),
+                Staff.builder().id("user4@gmail.com").fullname("nguyễn văn user4").level(2).build(),
+                Staff.builder().id("user5@gmail.com").fullname("nguyễn văn user5").level(1).build(),
+                Staff.builder().id("user6@gmail.com").fullname("nguyễn văn user6").level(0).build());
+        model.addAttribute("list", list);
+        return "staff-list";
+    }
+
+    @RequestMapping("/staff/lists")
+    public String lists(Model model) {
+        List<Staff> list = List.of(
+                Staff.builder().id("user1@gmail.com").fullname("nguyễn văn user1").level(0).build(),
+                Staff.builder().id("user2@gmail.com").fullname("nguyễn văn user2").level(1).build(),
+                Staff.builder().id("user3@gmail.com").fullname("nguyễn văn user3").level(2).build(),
+                Staff.builder().id("user4@gmail.com").fullname("nguyễn văn user4").level(2).build(),
+                Staff.builder().id("user5@gmail.com").fullname("nguyễn văn user5").level(1).build(),
+                Staff.builder().id("user6@gmail.com").fullname("nguyễn văn user6").level(0).build());
+        model.addAttribute("list", list);
+        return "list-status";
+    }
+
+    @RequestMapping("/staff/listss")
+    public String listss(Model model) {
+        List<Staff> list = List.of(
+                Staff.builder().id("user1@gmail.com").fullname("nguyễn văn user1").level(0).build(),
+                Staff.builder().id("user2@gmail.com").fullname("nguyễn văn user2").level(1).build(),
+                Staff.builder().id("user3@gmail.com").fullname("nguyễn văn user3").level(2).build(),
+                Staff.builder().id("user4@gmail.com").fullname("nguyễn văn user4").level(2).build(),
+                Staff.builder().id("user5@gmail.com").fullname("nguyễn văn user5").level(1).build(),
+                Staff.builder().id("user6@gmail.com").fullname("nguyễn văn user6").level(0).build());
+        model.addAttribute("list", list);
+        return "list-controls";
+    }
+
+}
